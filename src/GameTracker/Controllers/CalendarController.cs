e4df435f75834b42385b12fbc1c6d6fdbe735eb1
Erik <erikmickels@gmail.com>
2017-06-09 13:20:14 -0500

    Calendar navigation works

--- src/GameTracker/Controllers/CalendarController.cs
+++ src/GameTracker/Controllers/CalendarController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,11 +12,19 @@ namespace GameTracker.Controllers
     public class CalendarController : Controller
     {
         // GET: /<controller>/
-        public IActionResult Index(DateTime? sentDate = null)
+        public IActionResult Index(string id = null)
         {
             //according to Stackoverflow, default(DateTime) causing an exception here is a bug
 
-            if (sentDate == null)
+            DateTime sentDate = new DateTime();
+
+            if (id == null)
+                sentDate = DateTime.Today;
+            else
+                if (DateTime.TryParseExact(id, "MM-dd-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out sentDate))
+                //setting this to the first would maybe look better, seems to serve no purpose
+                return View(sentDate);
+                else
                 sentDate = DateTime.Today;
 
             return View(sentDate);
